1405fcb655282c3a1421305a7c48d4fe04db6459
Juan Antonio <56176388+JUANAN2019@users.noreply.github.com>
2024-05-05 17:29:57 +0200

    Update WIKJR123.java

--- artifact/libraries/WIKJR123/src/main/java/com/bbva/wikj/lib/r123/WIKJR123.java
+++ artifact/libraries/WIKJR123/src/main/java/com/bbva/wikj/lib/r123/WIKJR123.java
@@ -3,18 +3,8 @@ package com.bbva.wikj.lib.r123;
 import com.bbva.wikj.dto.ejemploselect.CustomerIn;
 import com.bbva.wikj.dto.ejemploselect.CustomerOut;
 
-import java.util.Map;
 
-/**
- * The  interface WIKJR123 class...
- */
 public interface WIKJR123 {
-
-
 	int  executeInsert(CustomerIn customerIn);
 	CustomerOut executeSelect(String id);
-
-
-
-
 }
